ee9be30ec1f9448d366197e5706383dbfb64170d
Sabil49 <111126146+Sabil49@users.noreply.github.com>
2022-08-22 18:44:10 +0530

    Add files via upload

--- chatapplication/settings.py
+++ chatapplication/settings.py
@@ -74,6 +74,14 @@ ASGI_APPLICATION = 'chatapplication.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -144,9 +152,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+"""
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels.layers.InMemoryChannelLayer",
     }
 }
+"""
